9b89294e5d14f3506d600d2c7327b96b55e15b14
mat2uken <ku@yasagure.jp>
2025-06-03 16:39:51 +0900

    Initialize Project

---
+++ app/src/main/java/app/mat2uken/android/app/clearoutcamerapreview/MainActivity.kt
@@ -0,0 +1,47 @@
+package app.mat2uken.android.app.clearoutcamerapreview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.mat2uken.android.app.clearoutcamerapreview.ui.theme.ClearoutcamerapreviewTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ClearoutcamerapreviewTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ClearoutcamerapreviewTheme {
+        Greeting("Android")
+    }
+}
